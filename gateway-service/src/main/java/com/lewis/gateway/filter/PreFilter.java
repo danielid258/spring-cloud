11c78e6d576911992179a980534e94c1cc46281e
danielid258 <danielid258@163.com>
2018-11-06 19:40:21 +0800

    Initial commit

---
+++ gateway-service/src/main/java/com/lewis/gateway/filter/PreFilter.java
@@ -0,0 +1,38 @@
+package com.lewis.gateway.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Daniel on 2018/11/6.
+ */
+@Component
+public class PreFilter extends ZuulFilter {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.SERVLET_DETECTION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        return null;
+    }
+}
